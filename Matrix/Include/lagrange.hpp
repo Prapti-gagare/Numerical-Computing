54e8452db2c6002a38e85e932b9f17bcb9d0952a
Prapti-gagare <praptigagare11@gmail.com>
2026-04-15 10:34:33 +0530

    Folder

--- Matrix/Include/lagrange.hpp
+++ Matrix/Include/lagrange.hpp
@@ -1,16 +1,20 @@
 #ifndef LAGRANGE_HPP
 #define LAGRANGE_HPP
+
 #include "interpolation.hpp"
 #include <vector>
 #include <fstream>
+
 using namespace std;
+
 class Lagrange : public Interpolation
 {
 public:
     Lagrange();
-    Lagrange(const Matrix &m);
+    explicit Lagrange(const Matrix &m);   
     double basisPolynomial(int i, double x, ofstream &fout) const;
     vector<double> allBasisValues(double x) const;
     double interpolate(double xQuery, ofstream &fout) override;
 };
-#endif
+
+#endif 
--- Matrix/src/lagrange.cpp
+++ Matrix/src/lagrange.cpp
@@ -5,28 +5,29 @@
 #include <sstream>
 
 using namespace std;
-Lagrange::Lagrange() : Interpolation() {}
 
+Lagrange::Lagrange() : Interpolation() {}
 Lagrange::Lagrange(const Matrix &m) : Interpolation(m) {}
 double Lagrange::basisPolynomial(int i, double x, ofstream &fout) const
 {
-    int n = rows;
+    int n = getN();               
     if (i < 0 || i >= n)
         throw out_of_range("Lagrange::basisPolynomial: index i out of range.");
 
-    double xi = mat[i][0];
+    double xi = getX(i);            
 
     fout << "  L_" << i << "(x) = ";
+
     double num = 1.0, den = 1.0;
     bool firstFactor = true;
 
     for (int j = 0; j < n; j++)
     {
         if (j == i) continue;
-        double xj = mat[j][0];
+        double xj = getX(j);        
 
-        num *= (x   - xj);
-        den *= (xi  - xj);
+        num *= (x  - xj);
+        den *= (xi - xj);
 
         if (!firstFactor) fout << " * ";
         fout << "(x - " << xj << ")";
@@ -38,7 +39,7 @@ double Lagrange::basisPolynomial(int i, double x, ofstream &fout) const
     for (int j = 0; j < n; j++)
     {
         if (j == i) continue;
-        double xj = mat[j][0];
+        double xj = getX(j);
         if (!firstFactor) fout << " * ";
         fout << "(" << xi << " - " << xj << ")";
         firstFactor = false;
@@ -52,21 +53,26 @@ double Lagrange::basisPolynomial(int i, double x, ofstream &fout) const
     return Li;
 }
 
+
 vector<double> Lagrange::allBasisValues(double x) const
 {
-    int n = rows;
+    int n = getN();                 
     vector<double> L(n, 1.0);
+
     for (int i = 0; i < n; i++)
     {
-        double xi = mat[i][0];
+        double xi = getX(i);         
         for (int j = 0; j < n; j++)
         {
             if (j == i) continue;
-            double xj = mat[j][0];
+            double xj = getX(j);     
+
             double den = xi - xj;
             if (fabs(den) < 1e-15)
-                throw runtime_error("Lagrange: duplicate x values at indices "
-                                    + to_string(i) + " and " + to_string(j));
+                throw runtime_error(
+                    "Lagrange: duplicate x values at indices "
+                    + to_string(i) + " and " + to_string(j));
+
             L[i] *= (x - xj) / den;
         }
     }
@@ -75,32 +81,35 @@ vector<double> Lagrange::allBasisValues(double x) const
 
 double Lagrange::interpolate(double xQuery, ofstream &fout)
 {
-    validateDataPoints();
-    int n = rows;
+    validateDataPoints();           
+
+    int n = getN();                
+    fout << fixed << setprecision(6);
     fout << "       Lagrange Interpolation\n";
     fout << "Query point: x = " << xQuery << "\n\n";
 
-    printDataPoints(fout);
-
+    printDataPoints(fout);         
     for (int i = 0; i < n; i++)
-        if (fabs(mat[i][0] - xQuery) < 1e-12)
+    {
+        if (fabs(getX(i) - xQuery) < 1e-12)
         {
             fout << "x = " << xQuery
-                 << " is a data node. Exact value = " << mat[i][1] << "\n";
-            return mat[i][1];
+                 << " is a data node. Exact value = " << getY(i) << "\n";
+            return getY(i);         
         }
+    }
 
-    fout << "Formula:  P(x) = sum of [ y_i * L_i(x) ]  for i = 0 to " << n-1 << "\n\n";
+    fout << "Formula:  P(x) = sum of [ y_i * L_i(x) ]  for i = 0 to "
+         << n - 1 << "\n\n";
+
+    fout << "--- Computing basis polynomials at x = " << xQuery << " ---\n\n";
 
     double result = 0.0;
 
-    fout << fixed << setprecision(6);
-    fout << "--- Computing basis polynomials at x = " << xQuery << " ---\n\n";
-
     for (int i = 0; i < n; i++)
     {
-        double Li = basisPolynomial(i, xQuery, fout);
-        double yi = mat[i][1];
+        double Li   = basisPolynomial(i, xQuery, fout);  
+        double yi   = getY(i);                           
         double term = yi * Li;
 
         fout << "  y_" << i << " * L_" << i << "(" << xQuery << ")"
@@ -110,26 +119,27 @@ double Lagrange::interpolate(double xQuery, ofstream &fout)
         result += term;
     }
 
+    vector<double> Lvals = allBasisValues(xQuery);
     double sumL = 0.0;
-    auto Lvals = allBasisValues(xQuery);
     for (double v : Lvals) sumL += v;
 
     fout << "--- Summary ---\n";
     fout << "Sum of all L_i (should be 1.0): " << sumL << "\n\n";
-fout << "Contribution of each term:\n\n";
-for (int i = 0; i < n; i++)
-{
-    double yi = mat[i][1];
-    double Li = Lvals[i];
-    double term = yi * Li;
 
-    fout << "i = " << i
-         << " : y_" << i << " = " << yi
-         << ", L_" << i << "(x) = " << Li
-         << ", y_" << i << " * L_" << i << "(x) = " << term << "\n";
-}
-fout << "\n";
-    
+    fout << "Contribution of each term:\n\n";
+    for (int i = 0; i < n; i++)
+    {
+        double yi   = getY(i);       
+        double Li   = Lvals[i];
+        double term = yi * Li;
+
+        fout << "i = " << i
+             << " : y_" << i << " = " << yi
+             << ", L_" << i << "(x) = " << Li
+             << ", y_" << i << " * L_" << i << "(x) = " << term << "\n";
+    }
+    fout << "\n";
+
     fout << "  P(" << xQuery << ") = " << result << "\n";
 
     return result;
